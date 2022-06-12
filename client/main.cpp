9fd6a8189081e935a5305e22e3cc7ce0e638b5d8
StepV1ad <96477530+StepV1ad@users.noreply.github.com>
2022-06-12 21:29:12 +0700

    added new file

--- client/main.cpp
+++ client/main.cpp
@@ -20,7 +20,6 @@ int main()
 	else if (IsWindowsVistaOrGreater())  std::cout << "Vista" << std::endl;
 	else if (IsWindowsXPOrGreater())  std::cout << "XP" << std::endl;
 #endif
-
 	Chat chat;
 	chat.start();
 	while (chat.chatWork())
